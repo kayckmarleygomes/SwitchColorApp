f00f9937cfb94415bbb09ac09d3993efc0faef4d
Kayck Marley <128845286+kayckmarleygomes@users.noreply.github.com>
2023-12-01 22:00:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+
+
+button.addEventListener('click',changeBg)
+
+function changeBg(){
+    var x = Math.floor(Math.random()*256);
+    var y = Math.floor(Math.random()*256);
+    var z = Math.floor(Math.random()*256);
+    var bgColor = `rgb(${x},${y},${z})`;
+    console.log(bgColor);
+    document.body.style.backgroundColor=bgColor;
+}
